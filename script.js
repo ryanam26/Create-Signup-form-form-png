19f4a49e9f4090d8c25f4fcad6d7c1f81bf30be9
Ryan Morrison <ryanam26@yahoo.com>
2020-03-31 16:33:48 -0400

    adding form validation

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 const form = document.getElementById('form');
 
+
 form.addEventListener('submit', e => {
     e.preventDefault();
-})
+
+    const firstName = form['firstname'].value;
+    const lastName = form['lastname'].value;
+    const email = form['email'].value;
+    const password = form['password'].value;
+
+    if (firstName === "") {
+        addErrorTo('firstname', 'First Name is required');
+    }
+
+    if (lastName === "") {
+        addErrorTo('lastname', 'Last Name is required');
+    }
+
+    if (email === "") {
+        addErrorTo('email', 'Email is required');
+    }
+
+    if (isValid(email)) {
+        addErrorTo('email', 'email is not valid');
+    }
+
+    if (password === '') {
+        addErrorTo('password', 'Password is required');
+    }
+});
+
+function addErrorTo(field, message) {
+    const small = form[field].parentNode.querySelector('small')
+    small.innerText = message;
+    small.style.opacity = '1';
+}
+
+function isValid(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+}
